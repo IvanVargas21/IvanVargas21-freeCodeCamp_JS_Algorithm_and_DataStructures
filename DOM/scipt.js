612a5b9201d1827da1c2f8d4223bbb633559e3a1
ivanvargass <165545622+IvanVargas21@users.noreply.github.com>
2024-10-19 13:44:14 +0800

    Added new projects

--- DOM/scipt.js
+++ DOM/scipt.js
@@ -3,8 +3,9 @@
           Object{} that represents the page you see in the web browser
           and provide you with an API to interact with it.
 
-          Web browser constricts the DOM when it load an HTML document,
+          Web browser constructs the DOM when it load an HTML document,
           and structures all the elements in a tree-like representation.
+          
           JavaScript can access the DOM to dynamically
           change the content, structure, and style of the webpage.
 */
